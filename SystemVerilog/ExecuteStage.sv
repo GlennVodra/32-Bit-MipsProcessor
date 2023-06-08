c47a5d890527e24a08011f1581fe6a0aa397caea
Glenn Vodra <37476686+GlennVodra@users.noreply.github.com>
2023-06-07 20:54:04 -0400

    Added Code

--- SystemVerilog/ExecuteStage.sv
+++ SystemVerilog/ExecuteStage.sv
@@ -9,4 +9,46 @@
 
  Description : Execute Stage
 ----------------------------------------------------*/
-//TODO
+module ExecuteStage (RegWrite, MemToReg, MemWrite, ALUControl, ALUSrc, RegDst, RegSrcA, RegSrcB, 
+					 RtDest, RdDest, ImmIn, RegWriteOut, MemToRegOut, MemWriteOut, ALUResult, WriteData, WriteReg);
+	input logic RegWrite, MemToReg, MemWrite, ALUSrc, RegDst;
+	input logic [3:0] ALUControl;
+	input logic [31:0] RegSrcA, RegSrcB, ImmIn;
+	input logic [4:0] RtDest, RdDest;
+	
+	output logic RegWriteOut, MemToRegOut, MemWriteOut;
+	output logic [31:0] ALUResult, WriteData;
+	output logic [4:0] WriteReg;
+	
+	logic [31:0] Second_ALU_Input;
+	
+	alu32 MIPS_ALU (
+		.A(RegSrcA),
+		.B(Second_ALU_Input),
+		.OP(ALUControl),
+		.Y(ALUResult)
+	);
+	
+	always_comb begin
+		unique case (ALUSrc)
+			1'b0:    Second_ALU_Input = RegSrcB;
+			1'b1:    Second_ALU_Input = ImmIn;
+			default: Second_ALU_Input = '{32{0}};
+		endcase
+	end
+	
+	always_comb begin
+		unique case (RegDst)
+			1'b0:    WriteReg = RtDest;
+			1'b1:    WriteReg = RdDest;
+			default: WriteReg = '{5{0}};
+		endcase
+	end
+	
+	assign WriteData = RegSrcB;
+	
+	assign RegWriteOut = RegWrite;
+	assign MemToRegOut = MemToReg;
+	assign MemWriteOut = MemWrite;
+	
+endmodule
