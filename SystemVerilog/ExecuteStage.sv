dbba446a513181663553b8f663992a3031cd78df
Glenn Vodra <37476686+GlennVodra@users.noreply.github.com>
2023-06-12 18:34:19 -0400

    Proper Shift Support

--- SystemVerilog/ExecuteStage.sv
+++ SystemVerilog/ExecuteStage.sv
@@ -9,30 +9,41 @@
 
  Description : Execute Stage
 ----------------------------------------------------*/
-module ExecuteStage (RegWrite, MemToReg, MemWrite, ALUControl, ALUSrc, RegDst, RegSrcA, RegSrcB, 
-					 RtDest, RdDest, ImmIn, RegWriteOut, MemToRegOut, MemWriteOut, ALUResult, WriteData, WriteReg);
-	input logic RegWrite, MemToReg, MemWrite, ALUSrc, RegDst;
+module ExecuteStage (RegWrite, MemToReg, MemWrite, ALUControl, ALUSrcA, ALUSrcB, RegDst, RegSrcA, RegSrcB, 
+					 RtDest, RdDest, ImmIn, Shamt, RegWriteOut, MemToRegOut, MemWriteOut, ALUResult, WriteData, WriteReg);
+	input logic RegWrite, MemToReg, MemWrite, ALUSrcA, RegDst;
+	input logic [1:0] ALUSrcB;
 	input logic [3:0] ALUControl;
 	input logic [31:0] RegSrcA, RegSrcB, ImmIn;
-	input logic [4:0] RtDest, RdDest;
+	input logic [4:0] RtDest, RdDest, Shamt;
 	
 	output logic RegWriteOut, MemToRegOut, MemWriteOut;
 	output logic [31:0] ALUResult, WriteData;
 	output logic [4:0] WriteReg;
 	
+	logic [31:0] First_ALU_Input;
 	logic [31:0] Second_ALU_Input;
 	
 	alu32 MIPS_ALU (
-		.A(RegSrcA),
+		.A(First_ALU_Input),
 		.B(Second_ALU_Input),
 		.OP(ALUControl),
 		.Y(ALUResult)
 	);
 	
 	always_comb begin
-		unique case (ALUSrc)
-			1'b0:    Second_ALU_Input = RegSrcB;
-			1'b1:    Second_ALU_Input = ImmIn;
+		unique case (ALUSrcA)
+			1'b1: First_ALU_Input = RegSrcB;
+			1'b0: First_ALU_Input = RegSrcA;
+			default: First_ALU_Input = RegSrcA;
+		endcase
+	end
+	
+	always_comb begin
+		unique case (ALUSrcB)
+			2'b00:    Second_ALU_Input = RegSrcB;
+			2'b01:    Second_ALU_Input = ImmIn;
+			2'b10:	  Second_ALU_Input = {{27{1'b0}}, {Shamt[4:0]}};
 			default: Second_ALU_Input = '{32{0}};
 		endcase
 	end
